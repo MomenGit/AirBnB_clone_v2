a1ffacf02c2aa406746c888b9f31100894d13bdb
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-17 19:04:55 +0300

    MySQL setup test (Task 4)

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- a script that prepares a MySQL server for the project
 
 -- A database hbnb_test_db
-CREATE DTATBASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- A new user hbnb_test (in localhost)
 -- The password of hbnb_test should be set to hbnb_test_pwd
@@ -11,6 +11,6 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
-GRANT SELECT PRIVILEGES ON ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
