3ccf3d7868d0c42f636a80f45c856d341daaefb2
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-19 15:25:19 +0300

    Write reload function and change os import to import getenv only

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,8 +2,8 @@
 """"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-import os
+from sqlalchemy.orm import sessionmaker, scoped_session
+from os import getenv
 
 
 class DBStorage():
@@ -12,15 +12,25 @@ class DBStorage():
     __session = None
 
     def __init__(self) -> None:
+
+        if (getenv('HBNB_ENV') == 'test'):
+            pass
+
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
-                os.getenv('HBNB_MYSQL_USER'),
-                os.getenv('HBNB_MYSQL_PWD'),
-                os.getenv('HBNB_MYSQL_HOST'),
-                os.getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+                getenv('HBNB_MYSQL_USER'),
+                getenv('HBNB_MYSQL_PWD'),
+                getenv('HBNB_MYSQL_HOST'),
+                getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
 
     def all(self, cls=None):
-        pass
+        """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            cls_objects = {}
+            for key, val in self.__session.query(cls).all():
+                if cls == val.__class__:
+                    cls_objects[key] = val
+            return cls_objects
 
     def new(self, obj):
         pass
@@ -32,4 +42,17 @@ class DBStorage():
         pass
 
     def reload(self):
-        pass
+        from models.base_model import Base
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
