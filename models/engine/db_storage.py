f8a3dec80688db56dbf9ac70191aefe1acc40d0c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-20 14:13:06 +0300

    Add droping tables if HBNB_ENV is test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,15 +13,24 @@ class DBStorage():
 
     def __init__(self) -> None:
 
-        if (getenv('HBNB_ENV') == 'test'):
-            pass
-
         self.__engine = create_engine(
             'mysql+mysqldb://{}:{}@{}/{}'.format(
                 getenv('HBNB_MYSQL_USER'),
                 getenv('HBNB_MYSQL_PWD'),
                 getenv('HBNB_MYSQL_HOST'),
                 getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+
+        if getenv('HBNB_ENV') == 'test':
+            from models.base_model import Base
+            from models.base_model import BaseModel
+            from models.user import User
+            from models.place import Place
+            from models.state import State
+            from models.city import City
+            from models.amenity import Amenity
+            from models.review import Review
+
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
