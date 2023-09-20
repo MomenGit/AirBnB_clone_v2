e536884047c518f076c679b030469377ed93e3b9
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-20 16:33:25 +0300

    Add genenv check before the relationship

--- models/review.py
+++ models/review.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -13,4 +14,5 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
-    place = relationship('Place', back_populates='reviews')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        place = relationship('Place', back_populates='reviews')
