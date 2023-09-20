b94c21d0b38749c143932afae2cc5b6a6bd9feef
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-20 15:19:35 +0300

    Add: `FileStorage` relationship between `Place` and `Review`

--- models/place.py
+++ models/place.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
+from models.review import Review
+from models.amenities import Amenity
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
@@ -28,8 +30,24 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
-    reviews = relationship('Review', back_populates='place',
-                           cascade='all, delete, delete-orphan')
-    amenities = relationship(
-        'Amenity', secondary=place_amenity, viewonly=False,
-        back_populates='place_amenities')
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', back_populates='place',
+                               cascade='all, delete, delete-orphan')
+        amenities = relationship(
+            'Amenity', secondary=place_amenity, viewonly=False,
+            back_populates='place_amenities')
+    else:
+        @property
+        def reviews(self):
+            """
+            eturns the list of Review instances
+            with place_id equals to the current Place.id
+            It will be the FileStorage relationship
+            between Place and Review
+            """
+            from models import storage
+            list_reviews = list()
+            for obj in storage.all(Review).values():
+                if obj.place_id == self.id:
+                    list_reviews.append(obj)
+            return (list_reviews)
