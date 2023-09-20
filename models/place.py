a39dbb3b95a3c46aafa2aec5987939bf9f6f0a39
Mohamed Adel <mohamed.adel.a@outlook.com>
2023-09-20 15:46:11 +0300

    Adding (getenv import)

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, Table, ForeignKey
 from sqlalchemy.orm import relationship
