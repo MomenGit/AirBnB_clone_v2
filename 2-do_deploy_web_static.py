83d14019c3961c5f7ed8c0b477a561c56b39b5cd
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-09 00:04:10 +0300

    Write 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,13 +16,13 @@ def do_deploy(archive_path):
         version_name = file_name.split('.')[0]
         version_path = "/data/web_static/releases/{}/".format(version_name)
         put(archive_path, "/tmp/")
-        sudo("mkdir -p {}".format(version_path))
-        sudo("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
-        sudo("rm /tmp/{}".format(file_name))
-        sudo("mv {0}web_static/* {0}".format(version_path))
-        sudo("rm -rf {}web_static".format(version_path))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s {} /data/web_static/current".format(version_path))
+        run("mkdir -p {}".format(version_path))
+        run("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {0}web_static/* {0}".format(version_path))
+        run("rm -rf {}web_static".format(version_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(version_path))
         return True
     except Exception as e:
         return False
