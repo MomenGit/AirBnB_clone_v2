d263755953facdfa2cdd85caf5ac593fce3410af
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-08 23:29:17 +0300

    Write 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines the function do_deploy"""
+from fabric.api import env, put, run, local, sudo
+from os.path import exists
+
+env.hosts = ['100.26.235.86	', '100.26.153.227']
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to hosting web servers"""
+    if not exists(archive_path):
+        return False
+
+    try:
+        file_name = archive_path.split('/')[-1]
+        version_name = file_name.split('.')[0]
+        version_path = "/data/web_static/releases/{}/".format(version_name)
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p {}".format(version_path))
+        sudo("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("mv {0}web_static/* {0}".format(version_path))
+        sudo("rm -rf {}web_static".format(version_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(version_path))
+        return True
+    except Exception as e:
+        return False
