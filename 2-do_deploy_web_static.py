1e6d2685da05fa0dab7e15df97fdabd527894a39
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-09 00:15:46 +0300

    Commented out hosts

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Defines the function do_deploy"""
-from fabric.api import env, put, run, local, sudo
+from fabric.api import env, put, sudo
 from os.path import exists
 
-env.hosts = ['100.26.235.86	', '100.26.153.227']
+# env.hosts = ['100.26.235.86', '100.26.153.227']
+# env.user = "ubuntu"
+# env.key = "~/.ssh/id_rsa_alx"
 
 
 def do_deploy(archive_path):
@@ -16,13 +18,14 @@ def do_deploy(archive_path):
         version_name = file_name.split('.')[0]
         version_path = "/data/web_static/releases/{}/".format(version_name)
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(version_path))
-        run("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}web_static/* {0}".format(version_path))
-        run("rm -rf {}web_static".format(version_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(version_path))
+        sudo("mkdir -p {}".format(version_path))
+        sudo("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("mv {0}web_static/* {0}".format(version_path))
+        sudo("rm -rf {}web_static".format(version_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(version_path))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Defines the function deploy"""
-from fabric.api import env, put, local, run
+from fabric.api import env, put, local, sudo
 from os.path import exists
 from datetime import datetime
 
-env.hosts = ['100.26.235.86	', '100.26.153.227']
+# env.hosts = ['100.26.235.86', '100.26.153.227']
+# env.user = "ubuntu"
+# env.key = "~/.ssh/id_rsa_alx"
 
 
 def do_pack():
@@ -33,13 +35,14 @@ def do_deploy(archive_path):
         version_name = file_name.split('.')[0]
         version_path = "/data/web_static/releases/{}".format(version_name)
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(version_path))
-        run("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {0}/web_static/* {0}/".format(version_path))
-        run("rm -rf {}/web_static".format(version_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(version_path))
+        sudo("mkdir -p {}".format(version_path))
+        sudo("tar -xzf /tmp/{} -C {}/".format(file_name, version_path))
+        sudo("rm /tmp/{}".format(file_name))
+        sudo("mv {0}/web_static/* {0}/".format(version_path))
+        sudo("rm -rf {}/web_static".format(version_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {}/ /data/web_static/current".format(version_path))
+        print("New version deployed!")
         return True
     except Exception as e:
         return False
