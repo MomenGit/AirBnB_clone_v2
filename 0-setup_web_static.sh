dba0f4887333b5b16a3401436cc58f58c9f1db93
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-08 17:43:03 +0300

    Write 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/shared
+mkdir -p /data/web_static/releases/test
+echo "<html>
+  <head>
+  </head>
+  <body>
+    ALX School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "/server_name _;/a \
+    \n\tlocation /hbnb_static/ {\
+        \n\t\talias /data/web_static/current/;\
+    \n\t}" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
