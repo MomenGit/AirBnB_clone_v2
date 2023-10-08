411d67fe1396bb8cd37b05b2f95d927d96fb4488
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-08 21:29:11 +0300

    Write 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Define the function do_pack"""
-from fabric import api
+from fabric import operations as ops
 from datetime import datetime
 
 
@@ -9,10 +9,10 @@ def do_pack():
     generates a .tgz archive from the contents of the web_static folder
     of your AirBnB Clone repo
     """
-    api.local("mkdir versions")
+    ops.local("mkdir versions")
     file_path = "versions/web_static_{}.tar".format(
         datetime.utcnow().strftime("%Y%m%d%H%M%S"))
-    tar_file = api.local(
+    tar_file = ops.local(
         "tar -cvzf {} ./web_static".format(file_path), capture=True)
     if tar_file.failed:
         return None
