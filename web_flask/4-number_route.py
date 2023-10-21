7d21080c3133311f108bd2a2043d5cfcab118ac3
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-21 21:48:58 +0300

    Write 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""A Flask web app that has routes '/' & '/hbnb'"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Returns “Hello HBNB!”"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Returns “HBNB”"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Returns “C ” followed by text"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Returns “Python ” followed by text"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Returns “n is a number”"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
