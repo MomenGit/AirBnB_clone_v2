081215032762e2d1e086b491d5439db4472c1c3c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-23 00:34:13 +0300

    Write 10-hbnb_filters.py

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Docs"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Returns an HTML page containing list of states"""
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def close_db_session(error):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
