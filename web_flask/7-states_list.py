fde0c46e2ddda6029e1874b51857bf304c7bdbc4
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-10-22 22:59:58 +0300

    Fix pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,8 @@
 """
 Defines /states_list: that displays an HTML page: (inside the tag BODY)
     H1 tag: “States”
-    UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+    UL tag: with the list of all State objects present in DBStorage
+    sorted by name (A->Z) tip
     LI tag: description of one State: <state.id>: <B><state.name></B>
 """
 from flask import Flask, render_template
@@ -15,12 +16,12 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Returns an HTML page containing list of states"""
-    states_list = storage.all(State).values()
-    return render_template("7-states_list.html", states=states_list)
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def close_db_session(error):
+def close_db_session(exception):
     """Remove the current SQLAlchemy Session"""
     storage.close()
 
